6fbf7d974ad6b01ad1cc0f5b2222c955520c453b
beka <mr.begzodjon96@mail.ru>
2020-05-26 21:31:55 +0300

    Validation for comments created

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,8 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post
+
+  validates :body, presence: true, length: { minimum: 10, maximum: 3000 }
+  validates :user_id, presence: true
+  validates :post_id, presence: true
 end
